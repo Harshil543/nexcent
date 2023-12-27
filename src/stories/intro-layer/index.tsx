0c0ee3da417f9ce1bd78a3ec00fd0dbc3c95f4b6
Harshil <sondagarharshil0@gmail.com>
2023-12-27 17:49:39 +0530

    css changes\.

--- src/stories/intro-layer/index.tsx
+++ src/stories/intro-layer/index.tsx
@@ -2,12 +2,12 @@ import styles from "./index.module.scss";
 
 const IntroLayer = ({ children }: any) => {
   return (
-    <>
+    <div className={styles.container}>
       <div className={styles.bgBox}>
         <div className="container-fluid">
           <div className="row">
-            <div className="col-lg-6"></div>
-            <div className="col-lg-6">
+            <div className="col-lg-6 h-100"></div>
+            <div className="col-lg-6 ">
               <div className={styles.loginBox}>
                 {children && { ...children }}
               </div>
@@ -15,7 +15,7 @@ const IntroLayer = ({ children }: any) => {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
